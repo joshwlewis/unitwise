d4ca1e7e34110bb42981cba903c174fb4ec578b8
Josh Lewis <josh.w.lewis@gmail.com>
2014-02-13 23:12:32 -0600

    Document composable.

--- lib/unitwise/composable.rb
+++ lib/unitwise/composable.rb
@@ -1,30 +1,48 @@
 module Unitwise
+  # Composable provides methods for determing the composition of unit, term, or 
+  # measurement. This is used to establish compatibility between units, terms,
+  # or measurements.
   module Composable
 
     def self.included(base)
       base.send :include, Comparable
     end
 
+    # A representation of a unit based on the atoms it's derived from.
+    # @return [SignedMultiset]
+    # @api public
     def composition
       root_terms.reduce(SignedMultiset.new) do |s, t|
         s.increment(t.atom.dim, t.exponent) if t.atom; s
       end
     end
 
+    # Define a default #dim for included classes.
+    # @return [String]
+    # @api public
     def dim
       composition_string
     end
 
+    # A string representation of a unit based on the atoms it's derived from
+    # @return [String]
+    # @api public
     def composition_string
       composition.sort.map do |k,v|
         v == 1 ? k.to_s : "#{k}#{v}"
       end.join('.')
     end
 
+    # Determine if this instance is similar to or compatible with other
+    # @return [true false]
+    # @api public
     def similar_to?(other)
       self.composition == other.composition
     end
 
+    # Compare whether the instance is greater, less than or equal to other.
+    # @return [-1 0 1]
+    # @api public
     def <=>(other)
       if other.respond_to?(:composition) && similar_to?(other)
         scalar <=> other.scalar
