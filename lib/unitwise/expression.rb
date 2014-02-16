6729e22b8c10fbbe518f9cc47aca8a6bf3649961
Josh Lewis <josh.w.lewis@gmail.com>
2014-02-15 20:29:59 -0600

    Add documentation

--- lib/unitwise/expression.rb
+++ lib/unitwise/expression.rb
@@ -7,12 +7,28 @@ require 'unitwise/expression/composer'
 require 'unitwise/expression/decomposer'
 
 module Unitwise
+  # The Expression module encompases all functions around encoding and decoding
+  # strings into Measurement::Units and vice-versa.
   module Expression
     class << self
+      # Build a string representation of a collection of terms
+      # @param terms [Array]
+      # @return [String]
+      # @example
+      #   Unitwise::Expression.compose(terms) # => "m2/s2"
+      # @api public
       def compose(terms)
         Composer.new(terms).expression
       end
 
+      # Convert a string representation of a unit, and turn it into a
+      # an array of terms
+      # @param expression [String] The string you wish to convert
+      # @return [Array]
+      # @example
+      #   Unitwise::Expression.decompose("m2/s2") 
+      #   # => [<Unitwise::Term m2>, <Unitwise::Term s-2>]
+      # @api public
       def decompose(expression)
         expression = expression.to_s
         @decompose ||= {}
--- lib/unitwise/standard.rb
+++ lib/unitwise/standard.rb
@@ -8,6 +8,10 @@ require 'unitwise/standard/scale'
 require 'unitwise/standard/function'
 
 module Unitwise
+  # The Standard module is responsible for fetching the UCUM specification unit
+  # standards and translating them into yaml files. This code is only used for
+  # by the rake task `rake unitwise:update_standard` and as such is not
+  # normally loaded.
   module Standard
     HOST = "unitsofmeasure.org"
     PATH = "/ucum-essence.xml"
