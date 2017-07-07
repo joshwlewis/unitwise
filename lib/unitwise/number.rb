a77514ac5ab4688bf9d8b2df95ea22aeb824bbd7
Josh W Lewis <josh.w.lewis@gmail.com>
2017-07-07 10:00:31 -0500

    Add Number.coefficify

--- lib/unitwise/number.rb
+++ lib/unitwise/number.rb
@@ -4,6 +4,7 @@ module Unitwise
     # it's value. For instance a value of 1.0 would return 1, a value of
     # #<BigDecimal:7f9558d559b8,'0.45E1',18(18)> would return 4.5.
     # @api public
+    # @param value [Integer, Float, Rational, String, BigDecimal]
     # @return [Integer, Float, Rational, BigDecimal]
     def self.simplify(value)
       case value
@@ -32,5 +33,17 @@ module Unitwise
       end
     end
 
+    # Coerces a string-like number to a BigDecimal or Integer as appropriate
+    # @api public
+    # @param value Something that can be represented as a string number
+    # @return [Integer, BigDecimal]
+    def self.coefficify(value)
+      d = BigDecimal.new(value.to_s)
+      if (i = d.to_i) == d
+        i
+      else
+        d
+      end
+    end
   end
 end
--- test/unitwise/number_test.rb
+++ test/unitwise/number_test.rb
@@ -1,7 +1,7 @@
 require 'test_helper'
 
 describe Unitwise::Number do
-  describe "#simplify" do
+  describe ".simplify" do
     it "simplifies to an Integer" do
       [2, 2.0, BigDecimal.new(2), Rational(2, 1), '2', '2.0'].each do |n|
         assert_equal Unitwise::Number.simplify(n), 2
@@ -25,6 +25,20 @@ describe Unitwise::Number do
     it "doesn't simplify complex Rationals" do
       r = Rational(22, 7)
       assert_equal Unitwise::Number.simplify(r), r
+    end
+  end
+
+  describe ".coefficify" do
+    it "converts integer values to Integers" do
+      [1, 1.0, "1.0", "1"].each do |n|
+        assert Unitwise::Number.coefficify(n).eql?(1)
+      end
+    end
+
+    it "converts decimal values to BigDecimals" do
+      [1.5, "1.5", "1.50", "0.15e1"].each do |n|
+        assert Unitwise::Number.coefficify(n).eql?(BigDecimal.new("1.5"))
+      end
     end
   end
 end
